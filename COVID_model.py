1c5f0772063fe6f93fea8c0063ff6c5025a262b5
Narjiss Sallahi <72303318+SallahiNarjiss@users.noreply.github.com>
2020-10-26 17:15:32 +0100

    Update COVID_model.py

--- COVID_model.py
+++ COVID_model.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle 
 
 df = pd.read_csv('covid19.csv', delimiter=";")
+oversample = SMOTE()
 
 #from sklearn.preprocessing import LabelEncoder
 
@@ -22,51 +23,56 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 6)
 
 
-#import sklearn
-#from sklearn.linear_model import LogisticRegression
-#from sklearn.metrics import precision_score, recall_score, f1_score
+oversample = SMOTE()
+X_train, y_train = oversample.fit_resample(X_train, y_train)
+X_train.shape
 
 
-#lr= LogisticRegression()
-#lr.fit(X_train,y_train)
+import sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-#print('Precision:',precision_score(y_test, lr.predict(X_test), average='macro'))
-#print('Recall:',recall_score(y_test, lr.predict(X_test), average='macro'))
-#print('F1 score:',f1_score(y_test, lr.predict(X_test), average='macro'))
 
-#from sklearn.svm import SVC,LinearSVC
-#svm = SVC()
-#svm.fit(X_train,y_train)
+lr= LogisticRegression()
+lr.fit(X_train,y_train)
 
-#print('Precision:',precision_score(y_test, svm.predict(X_test), average='macro'))
-#print('Recall:',recall_score(y_test, svm.predict(X_test), average='macro'))
-#print('F1 score:',f1_score(y_test,svm.predict(X_test), average='macro'))
+print('Precision:',precision_score(y_test, lr.predict(X_test), average='macro'))
+print('Recall:',recall_score(y_test, lr.predict(X_test), average='macro'))
+print('F1 score:',f1_score(y_test, lr.predict(X_test), average='macro'))
+
+from sklearn.svm import SVC,LinearSVC
+svm = SVC()
+svm.fit(X_train,y_train)
+
+print('Precision:',precision_score(y_test, svm.predict(X_test), average='macro'))
+print('Recall:',recall_score(y_test, svm.predict(X_test), average='macro'))
+print('F1 score:',f1_score(y_test,svm.predict(X_test), average='macro'))
 
 from sklearn.ensemble import RandomForestClassifier
 rf =RandomForestClassifier(n_estimators=30)
 rf.fit(X_train,y_train)
 
-#print('Precision:',precision_score(y_test, rf.predict(X_test), average='macro'))
-#print('Recall:',recall_score(y_test, rf.predict(X_test), average='macro'))
-#print('F1 score:',f1_score(y_test,rf.predict(X_test), average='macro'))
+print('Precision:',precision_score(y_test, rf.predict(X_test), average='macro'))
+print('Recall:',recall_score(y_test, rf.predict(X_test), average='macro'))
+print('F1 score:',f1_score(y_test,rf.predict(X_test), average='macro'))
 
-#from xgboost import XGBClassifier
-#model= XGBClassifier()
-#model.fit(X_train,y_train)
+from xgboost import XGBClassifier
+model= XGBClassifier()
+model.fit(X_train,y_train)
 
-#print('Precision:',precision_score(y_test, model.predict(X_test), average='macro'))
-#print('Recall:',recall_score(y_test, model.predict(X_test), average='macro'))
-#print('F1 score:',f1_score(y_test,model.predict(X_test), average='macro'))
+print('Precision:',precision_score(y_test, model.predict(X_test), average='macro'))
+print('Recall:',recall_score(y_test, model.predict(X_test), average='macro'))
+print('F1 score:',f1_score(y_test,model.predict(X_test), average='macro'))
 
 
-#from sklearn.neighbors import KNeighborsClassifier
-#vneigh = KNeighborsClassifier(n_neighbors=3)
-#vneigh.fit(X_train,y_train)
-#from sklearn.metrics import balanced_accuracy_score
-#print('Precision:',precision_score(y_test, neigh.predict(X_test), average='macro'))
-#print('Recall:',recall_score(y_test, neigh.predict(X_test), average='macro'))
-#print('F1 score:',f1_score(y_test,neigh.predict(X_test), average='macro'))
-#print('balanced accuracy score:',balanced_accuracy_score(y_test,neigh.predict(X_test)))
+from sklearn.neighbors import KNeighborsClassifier
+vneigh = KNeighborsClassifier(n_neighbors=3)
+vneigh.fit(X_train,y_train)
+from sklearn.metrics import balanced_accuracy_score
+print('Precision:',precision_score(y_test, neigh.predict(X_test), average='macro'))
+print('Recall:',recall_score(y_test, neigh.predict(X_test), average='macro'))
+print('F1 score:',f1_score(y_test,neigh.predict(X_test), average='macro'))
+print('balanced accuracy score:',balanced_accuracy_score(y_test,neigh.predict(X_test)))
 
 
 
